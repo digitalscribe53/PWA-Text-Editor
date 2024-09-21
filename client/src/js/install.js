eb85176732ea89750a08bd8e2c302662e089d02f
Kent Ball <kent.vincent.ball@gmail.com>
2024-09-21 09:33:10 -0400

    Updated webpack.config.js, src-sw.js, database.js

---
+++ client/src/js/install.js
@@ -0,0 +1,23 @@
+const butInstall = document.getElementById('buttonInstall');
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = 'block';
+});
+
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    deferredPrompt = null;
+    butInstall.style.display = 'none';
+  }
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA was installed');
+});
